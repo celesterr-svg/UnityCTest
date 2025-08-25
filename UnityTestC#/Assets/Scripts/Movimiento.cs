ea1a602cadc40e4482350494b816fe97b2f01fa7
celesterr-svg <celeste.rr@gmail.com>
2025-08-25 20:53:12 -0300

    V1

--- UnityTestC#/Assets/Scripts/Movimiento.cs
+++ UnityTestC#/Assets/Scripts/Movimiento.cs
@@ -7,12 +7,14 @@ public class Movimiento : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
     }
 }
